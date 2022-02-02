e7c02d87d396ae194dfae2f81a1f4c43e39af650
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2022-02-02 00:50:22 -0800

    developed nav

---
+++ ag-web-dev/src/Components/NavBar/NavElements.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const NavBarContainer = styled.nav`
+background: #000;
+position: fixed; 
+display: flex;
+width: 100%; 
+height: 80px;
+text-align: center;
+`
---
+++ ag-web-dev/src/Components/NavBar/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { NavBarContainer } from "./NavElements";
+export const NavBar = () => {
+  return (
+    <NavBarContainer>
+      <h1>Hello</h1>
+    </NavBarContainer>
+  );
+};
