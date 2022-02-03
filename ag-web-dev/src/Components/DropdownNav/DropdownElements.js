59d042298f9bf1b5d80d4080a4476c6466b84b00
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2022-02-02 22:27:57 -0800

    Created Dropdown Menu

--- ag-web-dev/src/Components/DropdownNav/DropdownElements.js
+++ ag-web-dev/src/Components/DropdownNav/DropdownElements.js
@@ -1,49 +1,54 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const DropdownWrapper=styled.div`
-    background: blue;
-    position: fixed;
-    top:0;
-    left:0;
-    width: 100%;
-    height: 100%;
-    display: grid;
-    grid-template-columns: repeat (4,1fr);
-    grid-auto-rows: minmax(100px, auto);
-    // grid-gap: 10px;
-    grid-template-areas: 
-    ". . . nav"
-    ". . . ."
-    ". list list ."
-    ". list list ."
-    ". list list ."
-    ". . . .";
-    z-index: 999;
+export const DropdownWrapper = styled.div`
+  background: black;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  align-items: center;
+  grid-template-columns: repeat (4, 1fr);
+  grid-auto-rows: minmax(100px, auto);
+  // grid-gap: 10px;
+  grid-template-areas:
+  ". . . nav"
+  ". . . ."
+  ". list list ."
+  ". list list ."
+  ". list list ."
+  ". . . .";
+  opacity: 96%;
+  z-index: 999;
 `
 
-export const Nav=styled.div`
-grid-area: nav;
-background: gray;
-// padding: 30px
+export const Nav = styled.div`
+  grid-area: nav;
+  background: gray;
+  justify-content: center;
+  text-align: center;
+  // padding: 30px
 `
 
-export const CloseIcon=styled.button`
-padding:0;
-margin:0;
-justify-content: center;
-font-size: 24px;`
-
-export const List= styled.div`
-grid-area: list;
-background: red;
-justify-content: center;
-// padding: 30px;
-display: grid;
-grid-gap: 10px
+export const CloseIcon = styled.button`
+  padding: 0;
+  margin: 0;
+  font-size: 24px;
 `
 
-export const Link=styled.p`
-    color: pink;
-    background: gray;
-    padding: 30px
+export const List = styled.div`
+  grid-area: list;
+//   background: red;
+  justify-content: center;
+  // padding: 30px;
+  display: grid;
+  grid-gap: 10px;
+`
+
+export const Link = styled.p`
+  color: white;
+//   background: gray;
+  padding: 30px;
+  font-size: 24px
 `
