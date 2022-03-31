03a08d362a5fae2663af66984701568c9a7b8dd1
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2022-03-31 02:25:59 -0700

    Updated info Section data

--- ag-web-dev/src/Components/InfoSection/data.js
+++ ag-web-dev/src/Components/InfoSection/data.js
@@ -13,10 +13,10 @@ export const sectionTwo = {
   id: "aboutmetwo",
   src: require("../../Svgs/careergoals.svg").default,
   alt: "Caption",
-  header: "Career Aspirations?",
+  header: "Career Aspirations",
   subtitle: "How ambitious am I?",
   content:
-    "Passionate and solutions-driven Full Stack Web Developer with a background in data modeling, optimization, and inventory management. Looking to develop autonomous business management solutions and improve current database management systems. Driven, collaborative, and effective leader with a proven ability to thrive in team environments. My most recent project was a plant tracking application built using MongoDB, React, and GraphQL. Hoping to leverage my software and business skills to create innovative tools.",
+    "They say Rome wasn't built overnight, but I'm not afraid to start building. Within the tech industry, I aspire to become a senior software developer by the end of 2025. In the meantime, I anticipate obtaining certifications as an AWS Cloud Practicioner, Certified Financial Analyst, and Project Manager. In the long-term, I will be a mentor to other first-generation Mexican Americans with a dream. I hope to call them fellow CEOs.",
   svgfirst: false,
 };
 
@@ -25,8 +25,8 @@ export const sectionThree = {
   src: require("../../Svgs/bootcamp.svg").default,
   alt: "Caption",
   header: "Full Stack Coding Bootcamp",
-  subtitle: "Certificate from UT December 2021",
-  content: "Passionate and solutions-driven Full Stack Web Developer with a background in data modeling, optimization, and inventory management. Looking to develop autonomous business management solutions and improve current database management systems. Driven, collaborative, and effective leader with a proven ability to thrive in team environments. My most recent project was a plant tracking application built using MongoDB, React, and GraphQL. Hoping to leverage my software and business skills to create innovative tools.",
+  subtitle: "Certificate from UT - December 2021",
+  content: "Worked on over 18 small projects, wrote my first gist, and collaborated on three applications with a talented team of developers (all three projects pinned below). I still remember how tough it was to write my first code quiz, and how it was even more challenging to style it after spending hours writing a script. I may have learned HTML, CSS, Javascript, Express, SQL, React, MongoDB, RESTful APIs and other highly-valued skills in software development, but there's still Python, Swift, and other languages and technologies to learn.",
   svgfirst: true,
 };
 
