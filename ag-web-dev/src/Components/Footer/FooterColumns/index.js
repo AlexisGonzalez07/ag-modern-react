6d67f7c48246d3e0de84bcdbf96a13f416aba727
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2022-03-31 02:04:22 -0700

    added hyperlinks to footer sections with nonstyled anchor tag

--- ag-web-dev/src/Components/Footer/FooterColumns/index.js
+++ ag-web-dev/src/Components/Footer/FooterColumns/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { ColumnWrapper, IconHolder, IconSubtitle, Img } from "./ColumnElements";
 
-export const FooterColumn = ({ src, alt, subtitle, position }) => {
+export const FooterColumn = ({ src, alt, subtitle, position,href }) => {
   return (
     <ColumnWrapper position={position}>
-      <IconHolder>
+      <a style={{all: 'inherit', cursor: 'pointer'}} href={href} target={'_blank'} rel='noreferrer'><IconHolder>
         <Img src={src} alt={alt} style={{fill: 'white'}}/>
       </IconHolder>
       <IconSubtitle>{subtitle}</IconSubtitle>
+      </a>
     </ColumnWrapper>
   );
 };
--- ag-web-dev/src/Components/Footer/data.js
+++ ag-web-dev/src/Components/Footer/data.js
@@ -6,12 +6,14 @@ export const first = {
   src: require("../../Svgs/linkedin.svg").default,
   subtitle: "LinkedIn",
   alt: "LinkedIn",
+  href:'https://www.linkedin.com/in/alexis-gonzalez-07/'
 };
 export const second = {
   position: 'second',
   src: require("../../Svgs/mail.svg").default,
   subtitle: "Email Me",
   alt: "Email",
+  href:'mailto:alexis.gonzalez07@gmail.com'
 
 };
 export const third = {
@@ -19,5 +21,5 @@ export const third = {
   src: require("../../Svgs/github.svg").default,
   subtitle: "Github",
   alt: "Github",
-
+href:'https://github.com/AlexisGonzalez07'
 };
